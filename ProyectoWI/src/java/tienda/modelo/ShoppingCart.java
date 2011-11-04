06940e2ac90120e6c2dee87ba34dc0b7af2daf1a
nessia.gp@gmail.com <nessia.gp@gmail.com@13373e78-4105-57f5-9a5f-0ffcc70f421e>
2011-11-04 16:22:02 +0000

    Modelo para os descontos

--- ProyectoWI/src/java/tienda/modelo/ShoppingCart.java
+++ ProyectoWI/src/java/tienda/modelo/ShoppingCart.java
@@ -1,80 +1,147 @@
 package tienda.modelo;
 
+import tienda.modelo.descuentos.Descuento;
 import java.util.*;
 //Clase que identifica la cesta de la compra
-public class ShoppingCart
+public class ShoppingCart extends Compra
 {
 
-    private ArrayList<Pedido> cesta;
-    private double precioTotal;
-
+    private HashMap<String,Pedido> cesta;
+    
     public ShoppingCart()
     {
-        cesta = new ArrayList<Pedido>();
+        super();
+        cesta = new HashMap<String, Pedido>();
+    }
+    
+    private void setCesta(HashMap<String,Pedido> cesta){
+        this.cesta = cesta;
     }
 
     public synchronized boolean up(Articulo pedido)
     {
         Pedido dp = new Pedido(pedido);
-        for (int i = 0; i < cesta.size(); i++)
-        {
-            if (cesta.get(i).getArticulo().getCodigoArticulo().equalsIgnoreCase(pedido.getCodigoArticulo()) &&
-                    cesta.get(i).getArticulo().getPrecio()==pedido.getPrecio())
-            {
-                if (pedido.getUnidades() > cesta.get(i).getCantidad())
-                {   
-                    cesta.get(i).setCantidad(cesta.get(i).getCantidad() + 1);
-                    return true;
-                }
-                else
-                {
-                    return false;
-                }
-
+        Pedido pedidoCesta = cesta.get(pedido.getCodigoArticulo());
+        if(pedidoCesta != null){
+            if(pedido.getUnidades() <= pedidoCesta.getCantidad()){
+                return false;
+            }
+            pedidoCesta.setCantidad(pedidoCesta.getCantidad() + 1);
+        }else{
+            cesta.put(pedido.getCodigoArticulo(), dp);
+        }
+        return true;
+    }
+    
+    
+    public synchronized boolean up(List<Articulo> pedidos){
+        for(int i=0; i<pedidos.size(); i++){
+            Articulo pedido = pedidos.get(i);
+            Pedido pedidoCesta = cesta.get(pedidos.get(i).getCodigoArticulo());
+            if (pedidoCesta != null && pedido.getUnidades() <= pedidoCesta.getCantidad())
+            {   
+                return false;
+            }  
+        }
+        for(int i=0; i<pedidos.size(); i++){
+            Pedido pedidoCesta = cesta.get(pedidos.get(i).getCodigoArticulo());
+            Pedido dp = new Pedido(pedidos.get(i));
+            if(pedidoCesta != null){
+                pedidoCesta.setCantidad(pedidoCesta.getCantidad() + 1);
+            }else{
+                cesta.put(dp.getArticulo().getCodigoArticulo(), dp);
             }
         }
-        cesta.add(dp);
         return true;
     }
 
-    public synchronized void down(Articulo pedido)
-    {
-        //ArticuloPedido dp = new ArticuloPedido(pedido);
-        for (int i = 0; i < cesta.size(); i++)
-        {
-            if (cesta.get(i).getArticulo().getCodigoArticulo().equalsIgnoreCase(pedido.getCodigoArticulo()))
-            {
-                cesta.get(i).setCantidad(cesta.get(i).getCantidad() - 1);
-                if (cesta.get(i).getCantidad() < 1)
-                {
-                    delete(cesta.get(i));
-                }
-                return;
+    public synchronized void down(Articulo pedido) {
+        Pedido pedidoCesta = cesta.get(pedido.getCodigoArticulo());
+        if(pedidoCesta != null){
+            pedidoCesta.setCantidad(pedidoCesta.getCantidad() - 1);
+            if(pedidoCesta.getCantidad() < 1){
+                delete(pedidoCesta);
             }
         }
-
     }
 
-    public synchronized void delete(Pedido dp)
-    {
-        cesta.remove(dp);
-
+    public synchronized void delete(Pedido dp) {
+        cesta.remove(dp.getArticulo().getCodigoArticulo());
     }
 
-    public double getPrecioTotal()
-    {
-        precioTotal = 0;
+    /**
+     * No incluye los descuentos de cesta, sólo los de pedido
+     * @return 
+     */
+    @Override
+    public double getPrecioSinDescuento(){
+        ArrayList<Pedido> array = getCesta();
+        double precioTotal = 0;
 
-        for (int i = 0; i < cesta.size(); i++)
+        for (int i = 0; i < array.size(); i++)
         {
-            precioTotal += cesta.get(i).getTotal();
+            precioTotal += array.get(i).getPrecioTotal();
         }
         return (Double) precioTotal;
 
     }
 
-    public ArrayList<Pedido> getCesta()
-    {
+    public ArrayList<Pedido> getCesta(){
+        return new ArrayList<Pedido>(cesta.values());
+    }
+
+    
+
+    /*public HashMap<String, Pedido> getCestaHash() {
         return cesta;
+    }*/
+    
+    public Pedido getPedido(String codigoArticulo){
+        return cesta.get(codigoArticulo);
+    }
+    
+    public boolean tieneArticulo(String codigoArticulo){
+        return cesta.containsKey(codigoArticulo);
+    }
+
+    @Override
+    public Object clone() throws CloneNotSupportedException {
+        ShoppingCart clon = new ShoppingCart();
+        //Clonamos la cesta
+        HashMap<String,Pedido> cestaClon = new HashMap<String, Pedido>();
+        Collection<Pedido> valores = cesta.values();
+        Iterator<Pedido> it = valores.iterator();
+        while(it.hasNext()){
+            Pedido pedidoClon = (Pedido)it.next().clone();
+            cestaClon.put(pedidoClon.getArticulo().getCodigoArticulo(), pedidoClon);
+        }
+        clon.setCesta(cestaClon);
+        //Clonamos los descuentos
+        for(int i=0;i<descuentos.size();i++){
+            Descuento descuentoClon = (Descuento)descuentos.get(i).clone();
+            descuentoClon.setCompra(clon);
+            descuentoClon.actualizar();
+            clon.getDescuentos().add(descuentoClon);
+        }
+        //Devolvemos el clon
+        return clon; 
+    }
+    
+    
+    @Override
+    public void integrarDescuentos() throws CloneNotSupportedException {
+        //Integramos los descuentos de cada pedido
+        ArrayList<Pedido> cestaArray = getCesta();
+        for(int i=0; i<cestaArray.size(); i++){
+            cestaArray.get(i).integrarDescuentosEnCopia();
+        }
+        //Integramos los descuentos propios
+        Collections.sort(getDescuentos());
+        for(int i=0; i<descuentos.size(); i++){
+            descuentos.get(i).integrarDescuento();
+        }
+        //Borramos los descuentos
+        descuentos = new ArrayList<Descuento>();
+
     }
 }
