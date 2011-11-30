5adc7c4f070d3bb25abb69c5d9883fd21720cf64
nessia.gp@gmail.com <nessia.gp@gmail.com@13373e78-4105-57f5-9a5f-0ffcc70f421e>
2011-11-30 11:51:01 +0000

    Parametrización del número de registros que se muestran en la administración

---
+++ ProyectoWI/src/java/tienda/Helpers/AbstractHelper.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tienda.Helpers;
+
+/**
+ *
+ * @author nessa
+ */
+public class AbstractHelper {
+    protected int registros = 6;
+}
